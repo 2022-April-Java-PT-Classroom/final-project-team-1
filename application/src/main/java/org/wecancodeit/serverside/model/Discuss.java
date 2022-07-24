4eb678614667273726f1c7d0a7a7447b789955a8
zibadcoder <emushmala@yahoo.com>
2022-07-24 15:41:10 -0400

    My first commit

--- application/src/main/java/org/wecancodeit/serverside/model/Discuss.java
+++ application/src/main/java/org/wecancodeit/serverside/model/Discuss.java
@@ -17,4 +17,48 @@ public class Discuss {
     @Lob
     private String discussAnswerOne;
     private String discussAnswerTwo;
+
+    public Discuss(){
+
+    }
+
+    public Discuss(String discussDate, String discussQuestion, String discussAnswerOne, String discussAnswerTwo) {
+        this.discussDate = discussDate;
+        this.discussQuestion = discussQuestion;
+        this.discussAnswerOne = discussAnswerOne;
+        this.discussAnswerTwo = discussAnswerTwo;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getDiscussDate() {
+        return discussDate;
+    }
+
+    public String getDiscussQuestion() {
+        return discussQuestion;
+    }
+
+    public String getDiscussAnswerOne() {
+        return discussAnswerOne;
+    }
+
+    public String getDiscussAnswerTwo() {
+        return discussAnswerTwo;
+    }
+
+    @Override
+    public String toString() {
+        return "Discuss{" +
+                "id=" + id +
+                ", discussDate='" + discussDate + '\'' +
+                ", discussQuestion='" + discussQuestion + '\'' +
+                ", discussAnswerOne='" + discussAnswerOne + '\'' +
+                ", discussAnswerTwo='" + discussAnswerTwo + '\'' +
+                '}';
+    }
+
+
 }
