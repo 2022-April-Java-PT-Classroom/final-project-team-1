0731a8da0e0a73062ddc99236771256f0316e401
noellegisewhite <noellegisewhite@gmail.com>
2022-07-25 20:55:03 -0400

    Homepage hero section

--- application/src/main/resources/static/js/react-client/src/Pages/Home-Page/index.js
+++ application/src/main/resources/static/js/react-client/src/Pages/Home-Page/index.js
@@ -5,7 +5,10 @@ const HomePage =() => {
     return (
         <div>
             <section className={style.homeHero}>
-                <h1 className={style.homeHeroH1}>Dfdsafdsdfdf</h1>
+                <div className={style.homeContainer}>
+                <h1 className={style.homeHeroH1}>Rekindle The Love Flame</h1>
+                <h2 className={style.homeHeroH2}>A Deeper Connection With Your Significant Other</h2>
+                </div>
             </section>
         </div>
     );
