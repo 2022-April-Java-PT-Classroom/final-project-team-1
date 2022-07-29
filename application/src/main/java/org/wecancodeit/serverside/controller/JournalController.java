356afe74fd188a8689fe8bb5418fbf86b34b2118
zimmchad <zimmermanchad@ymail.com>
2022-07-29 13:41:31 -0400

    journalcontrollerattempt2

--- application/src/main/java/org/wecancodeit/serverside/controller/JournalController.java
+++ application/src/main/java/org/wecancodeit/serverside/controller/JournalController.java
@@ -8,6 +8,7 @@ import org.wecancodeit.serverside.repository.JournalRepository;
 
 import javax.annotation.Resource;
 import java.util.Collection;
+import java.util.List;
 
 @RestController
 @CrossOrigin("*")
@@ -17,12 +18,18 @@ public class JournalController {
     @Resource
     private JournalRepository journalRepo;
 
+    @RequestMapping
+    public List<Journal> getAllJournal(){
+        return (List<Journal>) journalRepo.findAll();
+    }
+
     @PostMapping
     public Collection<Journal> addJournal(@RequestBody String body) throws JSONException {
         JSONObject newJournal=new JSONObject(body);
         String journalDate = newJournal.getString("date");
         String journalText = newJournal.getString("text");
 
+        return (Collection<Journal>) journalRepo.findAll();
     }
 
 }
