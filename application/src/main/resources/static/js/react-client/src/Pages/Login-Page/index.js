e7b3050989316abf5310f7e2a83b2a37e7b20a8c
noellegisewhite <noellegisewhite@gmail.com>
2022-07-31 00:59:29 -0400

    login/signup display

---
+++ application/src/main/resources/static/js/react-client/src/Pages/Login-Page/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import style from "./style.module.scss";
+
+const Login = () => {
+    return (
+        <div className={style.login}>
+            <h1>This is a login page</h1>
+
+        </div>
+    );
+}
+
+export default Login;
---
+++ application/src/main/resources/static/js/react-client/src/Pages/Signup-Page/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import style from "./style.module.scss";
+
+const Signup = () => {
+    return (
+        <div>
+            <h1>This is the signup page</h1>
+            <h1>This is the signup page</h1>
+            <h1>This is the signup page</h1>
+            <h1>This is the signup page</h1>
+        </div>
+    );
+}
+
+export default Signup;
