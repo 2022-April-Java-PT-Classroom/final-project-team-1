e9a5a42ed1a839c9a9a027e122a150540c5cfc6d
zimmchad <zimmermanchad@ymail.com>
2022-08-01 15:26:40 -0400

    promptcontrollerimports

--- application/src/main/java/org/wecancodeit/serverside/controller/PromptController.java
+++ application/src/main/java/org/wecancodeit/serverside/controller/PromptController.java
@@ -1,4 +1,27 @@
 package org.wecancodeit.serverside.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.wecancodeit.serverside.model.Prompt;
+import org.wecancodeit.serverside.repository.PromptRepository;
+import org.wecancodeit.serverside.repository.UserRepository;
+
+import javax.annotation.Resource;
+import java.util.Collection;
+import java.util.List;
+
+@RestController
+@CrossOrigin
+@RequestMapping
 public class PromptController {
+
+    @Resource
+    private PromptRepository promptRepo;
+
+    @Resource
+    private UserRepository userRepo;
+
+    
 }
