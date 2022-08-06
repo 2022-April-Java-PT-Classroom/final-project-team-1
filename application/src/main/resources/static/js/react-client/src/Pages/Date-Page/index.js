b89ef69fcfa97cb6c2f7b75c82ba068c9f92a025
Zaylet Feliciano <zayletfeliciano@Zaylets-MacBook-Pro.local>
2022-08-06 10:00:53 -0400

    working on wiring the date page with the backend

--- application/src/main/resources/static/js/react-client/src/Pages/Date-Page/index.js
+++ application/src/main/resources/static/js/react-client/src/Pages/Date-Page/index.js
@@ -3,43 +3,55 @@ import axios from 'axios';
 import style from './style.module.scss';
 import { Link } from "react-router-dom";
 
-const DatePage = () => {
+const DatePage = ({dateNight}) => {
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-
-        const userData = {
-            dateDate: "test",
-            dateIdea: "test",
-            dateType: "test",
-            dateLevel: "test",
-            dateNotes: "test"
-        };
-
-        axios.post('http://localhost:8080/dateNight/add-dateNight', userData).then((response) => {
-            console.log(response.status);
-            console.log('DATA', response.data);
-        });
-
-    };
+    const [dateNight, setDateNight] = useState(null);
+    const [loading, setLoading] = useSate(null);
 
     useEffect(() => {
         const fetchData = async () => {
-            const result = await axios('http://localhost:8080/dateNight');
-            console.log(result);
+            const dateNightIdea = await Axios('http://localhost:8080/dateNight/add-dateNight');
+            setDateNight(dateNightIdea.data);
+        };
+
+        if (dateNight) {
+            setLoading(false);
         }
 
+        const timer = setTimeout(() => {
+            !dateNight && fetchData();
+        }, 1000);
+        return () => clearTimeout(timer);
+    }, [dateNight]);
 
-        fetchData();
+    // const handleSubmit = (e) => {
+    //     e.preventDefault();
 
-        // eslint-disable-next-line
-    }, []);
+    //     axios.post('http://localhost:8080/dateNight/add-dateNight', userData).then((response) => {
+    //         console.log(response.status);
+    //         console.log('DATA', response.data);
+    //     });
+
+    // };
+
+    // useEffect(() => {
+    //     const fetchData = async () => {
+    //         const result = await axios('http://localhost:8080/dateNight');
+    //         console.log(result);
+    //     }
+
+
+    //     fetchData();
+
+    //     // eslint-disable-next-line
+    // }, []);
 
     return (
+        loading ? <h3>Creating Date Night Idea...</h3> :
         <div className={style.datePage}>
             <section className={style.dateMain}>
                 <h1 className={style.dateH1}>Date Ideas</h1>
-                <p className={style.datePara}></p>
+                <p className={style.datePara}>{dateNight.dateIdea}</p>
                 <form onSubmit={handleSubmit} className={style.dateForm}>
                     <input></input>
                     <input></input>
