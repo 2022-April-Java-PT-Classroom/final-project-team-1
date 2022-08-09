47a6400c933a9181d187366c9d80a6ca822dfb76
noellegisewhite <noellegisewhite@gmail.com>
2022-08-09 09:07:11 -0400

    quiz jsx

--- application/src/main/resources/static/js/react-client/src/Components/Quiz/index.js
+++ application/src/main/resources/static/js/react-client/src/Components/Quiz/index.js
@@ -1,69 +1,22 @@
 import React, { useState } from 'react';
+import Question from './Quiz-Questions';
+import Result from './Quiz-Results';
 import style from './style.module.scss';
 
 export default function Quiz() {
 
-	const questions = [
-		{
-			questionText: 'How long have you been together?',
-			answerOptions: [
-				{ answerText: '', isCorrect: false },
-				{ answerText: '', isCorrect: false },
-				{ answerText: '', isCorrect: true },
-				{ answerText: '', isCorrect: false },
-			],
-		},
-		{
-			questionText: 'Describe your personality',
-			answerOptions: [
-				{ answerText: '', isCorrect: false },
-				{ answerText: '', isCorrect: true },
-				{ answerText: '', isCorrect: false },
-				{ answerText: '', isCorrect: false },
-			],
-		},
-		{
-			questionText: 'Descibe your partners personality?',
-			answerOptions: [
-				{ answerText: '', isCorrect: true },
-				{ answerText: '', isCorrect: false },
-				{ answerText: '', isCorrect: false },
-				{ answerText: '', isCorrect: false },
-			],
-		},
-		{
-			questionText: 'Describe your typical dates?',
-			answerOptions: [
-				{ answerText: '', isCorrect: false },
-				{ answerText: '', isCorrect: false },
-				{ answerText: '', isCorrect: false },
-				{ answerText: '', isCorrect: true },
-			],
-		},
-	];
-
 	return (
-		<div>
-			{/* HINT: replace "false" with logic to display the 
-      score when the user has answered all the questions */}
-			{false ? (
-				<div className='score-section'>You scored 1 out of {questions.length}</div>
-			) : (
-				<>
-					<div className='question-section'>
-						<div className='question-count'>
-							<span>Question 1</span>/{questions.length}
-						</div>
-						<div className='question-text'>This is where the question text should go</div>
-					</div>
-					<div className='answer-section'>
-						<button>Answer 1</button>
-						<button>Answer 2</button>
-						<button>Answer 3</button>
-						<button>Answer 4</button>
-					</div>
-				</>
-			)}
+		<div className={style.quizCont}>
+			<div>
+				<h2>This is a quiz</h2>
+				<div>This is where the question text should go</div>
+			</div>
+				<div className={style.quizAns}>
+					<button className={style.quizBtn}>Answer 1</button>
+					<button className={style.quizBtn}>Answer 2</button>
+					<button className={style.quizBtn}>Answer 3</button>
+					<button className={style.quizBtn}>Answer 4</button>
+				</div>
 		</div>
 	);
 }
--- application/src/main/resources/static/js/react-client/src/Portal/Port-Pages/Port-Home/index.js
+++ application/src/main/resources/static/js/react-client/src/Portal/Port-Pages/Port-Home/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Quiz from "../../../Components/Quiz";
 import style from './style.module.scss'
 
 const PortalHome = () => {
@@ -6,6 +7,7 @@ const PortalHome = () => {
         <div className={style.portHomeMain}>
             <section className={style.portHome}>
             <h1 className={style.portH1}>This is the portal homepage page</h1>
+            <Quiz />
             </section>
         </div>
     )
