3a253d8735b34818df059f929ad23b2854ce4bd8
Zaylet Feliciano <zayletfeliciano@Zaylets-MacBook-Pro.local>
2022-08-10 01:30:03 -0400

    still working on it

--- application/src/main/resources/static/js/react-client/src/Pages/Date-Page/FormSubmission.js
+++ application/src/main/resources/static/js/react-client/src/Pages/Date-Page/FormSubmission.js
@@ -1,6 +1,6 @@
 import React, {useState} from "react";
 import axios from 'axios';
-import Style from '.style.module.scss';
+import style from './style.module.scss';
 
 const UserSubmmited = ({userSubmmited}) => {
 
@@ -37,8 +37,8 @@ const UserSubmmited = ({userSubmmited}) => {
         <div>
             <h3 className={style.dateTitle}>Tell us your experience on this date</h3>
                 <form className={style.dateForm} onSubmit={handleSubmit}>
-                    <input className={style.dateInput} type="date" name="dateDate" value={dateNight.dateDate} onChange={handleChange}/>
-                    <textarea className={style.userExperience} name="dateNotes" value={dateNight.dateNotes} onChange={handleChange} placeholder='Enter your experience'></textarea>
+                    <input className={style.dateInput} type="date" name="dateDate" value={dateDate} onChange={handleChange}/>
+                    <textarea className={style.userExperience} name="dateNotes" value={dateNotes} onChange={handleChange} placeholder='Enter your experience'></textarea>
                     <button className={style.dateBtn}>Submit</button>
                 </form>
         </div>
