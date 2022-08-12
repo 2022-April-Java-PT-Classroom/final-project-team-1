a2bc71e86edbe3a288d9a546eb14c81513f071db
zimmchad <zimmermanchad@ymail.com>
2022-08-12 11:04:22 -0400

    ignorerandomupdate

--- application/src/main/resources/static/js/react-client/src/Pages/Contact/index.js
+++ application/src/main/resources/static/js/react-client/src/Pages/Contact/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useHistory } from "react-router-dom";
 import style from "./style.module.scss";
 import FormPage from "../form/form";
+import { getBinaryDir } from "node-sass/lib/extensions";
 
 
 function Contact() {
@@ -12,18 +13,18 @@ function Contact() {
     }
 
     return (
-
-        <><><div className={style.card}>
-            <h2>Noelle Gisewhite</h2>
-            <div className={style.container}>
-                <h1><img className={style.Img} src="https://media-exp1.licdn.com/dms/image/C5603AQGkPSH6TY4kpw/profile-displayphoto-shrink_200_200/0/1651268817526?e=1663200000&v=beta&t=0tFxMxekMA6_5RIZs45y2xAsk4T3KPtS4e0KYxlGqoQ" alt="noellepic"></img>
-                </h1>
+        <><div className="grid-container">
+            <div className={style.card1}>
+                <h2>Noelle Gisewhite</h2>
+                <div className={style.container}>
+                    <h1><img className={style.Img} src="https://media-exp1.licdn.com/dms/image/C5603AQGkPSH6TY4kpw/profile-displayphoto-shrink_200_200/0/1651268817526?e=1663200000&v=beta&t=0tFxMxekMA6_5RIZs45y2xAsk4T3KPtS4e0KYxlGqoQ" alt="noellepic"></img>
+                    </h1>
+                </div>
+                <h4>Full Stack Developer</h4>
+                <a href="https://github.com/noellegisewhite">Github</a>
+                <a href="https://www.linkedin.com/in/noellegisewhite/">LinkedIn</a>
             </div>
-            <h4>Full Stack Developer</h4>
-            <a href="https://github.com/noellegisewhite">Github</a>
-            <a href="https://www.linkedin.com/in/noellegisewhite/">LinkedIn</a>
-        </div>
-            <div className={style.card}>
+            <div className={style.card2}>
                 <h2>Zibad Maldaye</h2>
                 <div className={style.container}>
                     <h1> <img className={style.Img} src="https://media-exp1.licdn.com/dms/image/C5603AQGog7mQ9N3D-Q/profile-displayphoto-shrink_200_200/0/1655995697589?e=1663200000&v=beta&t=6MAoLkQGEs7UIwJphXjgHP7azzWWwga4uRWtN8xuPQI" alt="zibadpic"></img>
@@ -33,7 +34,7 @@ function Contact() {
                 <a href="https://github.com/zimmchad">Github</a>
                 <a href="https://www.linkedin.com/in/zibad/">LinkedIn</a>
             </div>
-            <div className={style.card}>
+            <div className={style.card3}>
                 <h2>Zaylet Feliciano</h2>
                 <div className={style.container}>
                     <h1> <img classname={style.Img} src="https://media-exp1.licdn.com/dms/image/C4E03AQH0iHWQmxqFiQ/profile-displayphoto-shrink_400_400/0/1658684780805?e=1665014400&v=beta&t=Kb1qffqBbQ7Czhwr0UFz4UTcAJDPumf3hr_4ZFsOHEM" alt="zayletpic"></img>
@@ -43,7 +44,7 @@ function Contact() {
                 <a href="https://github.com/zaylet">Github</a>
                 <a href="https://www.linkedin.com/in/zayletfeliciano/">LinkedIn</a>
             </div>
-            <div className={style.card}>
+            <div className={style.card4}>
                 <h2>Gail Cowley</h2>
                 <div className={style.container}>
                     <h1> <img classname={style.Img} src="https://media-exp1.licdn.com/dms/image/C4D03AQGQczQs6yHrVQ/profile-displayphoto-shrink_400_400/0/1648436352349?e=1665014400&v=beta&t=7vcB9sgHwWyiVmpjHsIRc3o7E9a1AAKBOH8913NIxVk" alt="gailpic"></img>
@@ -53,7 +54,7 @@ function Contact() {
                 <a href="https://github.com/gailanne/">Github</a>
                 <a href="https://www.linkedin.com/in/11gailanne/">LinkedIn</a>
             </div>
-            <div className={style.card}>
+            <div className={style.card5}>
                 <h2>Joe Buckley</h2>
                 <div className={style.container}>
                     <h1><img classname={style.Img} src="https://media-exp1.licdn.com/dms/image/C5603AQFskh9Jh_DaNg/profile-displayphoto-shrink_400_400/0/1658208866396?e=1665014400&v=beta&t=PlGFoZT5OhIMkNwduccNxouFdc9lQ7syh9GmXxjCgik" alt="joepic"></img>
@@ -63,24 +64,25 @@ function Contact() {
                 <a href="https://github.com/joebuck125">Github</a>
                 <a href="https://www.linkedin.com/in/joe-buckley-4358561a2/">LinkedIn</a>
             </div>
-            <div className={style.card}>
-            <h2>Chad Zimmerman</h2>  
-          <div className={style.container}> 
-            <h1><img className={style.Img}src="https://media-exp1.licdn.com/dms/image/C4D03AQEOcd8Of0HR6A/profile-displayphoto-shrink_200_200/0/1650207672594?e=1663200000&v=beta&t=hPZrEEgpWwJFE5xtqLYDB4oWsngmDx-486EkNOW2Dvo" alt="chadpic"></img>
-              </h1>
-              </div> 
-              <h4>Full Stack Developer</h4>
-            <a href="https://github.com/zimmchad">Github</a>
-            <a href="https://www.linkedin.com/in/chad-zimmerman-4b4b35220/">LinkedIn</a>   
+            <div className={style.card6}>
+                <h2>Chad Zimmerman</h2>
+                <div className={style.container}>
+                    <h1><img className={style.Img} src="https://media-exp1.licdn.com/dms/image/C4D03AQEOcd8Of0HR6A/profile-displayphoto-shrink_200_200/0/1650207672594?e=1663200000&v=beta&t=hPZrEEgpWwJFE5xtqLYDB4oWsngmDx-486EkNOW2Dvo" alt="chadpic"></img>
+                    </h1>
+                </div>
+                <h4>Full Stack Developer</h4>
+                <a href="https://github.com/zimmchad">Github</a>
+                <a href="https://www.linkedin.com/in/chad-zimmerman-4b4b35220/">LinkedIn</a>
             </div>
-        </><div className={style.contactForm}>
+            <div className={style.contactForm}>
                 <section className={style.contactFormSection}>
                     <div className={style.contactFormContain}>
                         <h2 className={style.contactFormContainH2}>Contact ReKindle</h2>
                         <FormPage onEnteringComment={addContactInfo} />
                     </div>
                 </section>
-            </div></>
+            </div>
+        </div></>
       
     );
 }
