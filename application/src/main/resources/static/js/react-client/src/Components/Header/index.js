6fa2b6f906f2d4bd27425534d581577517b0419f
noellegisewhite <noellegisewhite@gmail.com>
2022-08-19 13:25:53 -0400

    header fix

--- application/src/main/resources/static/js/react-client/src/Components/Header/index.js
+++ application/src/main/resources/static/js/react-client/src/Components/Header/index.js
@@ -27,9 +27,9 @@ const Header = () => {
                     <NavLink className={style.navLink} to={'/AboutUs-Page'}>About</NavLink>
                     <NavLink className={style.navLink} to={'/Love-Page'}>Love Languages</NavLink>
                     <NavLink className={style.navLink} to={'/Quotes'}>Quotes</NavLink>
-                    {/* <NavLink className={style.navLink} to={'/Prompt-Page'}>Prompt</NavLink> */}
+                    {/* <NavLink className={style.navLink} to={'/Prompt-Page'}>Prompt</NavLink>
                     <NavLink className={style.navLink} to={'/Date-Page'}>Date</NavLink>
-                    {/* <NavLink className={style.navLink} to={'/Discuss-Page'}>Discuss</NavLink>
+                    <NavLink className={style.navLink} to={'/Discuss-Page'}>Discuss</NavLink>
                     <NavLink className={style.navLink} to={'/Journal-Page'}>Journal</NavLink> */}
                     <NavLink className={style.navLink} to={'/Resources'}>Resources</NavLink>
                     <NavLink className={style.navLink} to={'/Contact'}>Contact</NavLink>
